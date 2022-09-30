e0dfe2c9e4c4670e47515ca5b3f0dfd86fc50d7f
c7331642 <114752530+c7331642@users.noreply.github.com>
2022-09-30 14:56:59 +0100

    portfolio 1 week 1 cp krina

---
+++ portfolio1 cp 1 week krina.py
@@ -0,0 +1,22 @@
+# Task 1
+print("Hello World")
+# task 2
+print("Hello, krina!")
+# Task 3
+temperature_celsius = 38.4
+temperature_Fahrenheit= temperature_celsius*9/5+32
+print("Over the Summer ,temperatures in Yorkshire reached" , temperature_celsius , "is" , temperature_Fahrenheit )
+# task 4
+matches=609
+batted=1014
+nout=162
+runs=48426
+average=runs/(batted-nout)
+print("Boycott's batting average is" , average )
+# Task 5
+lg=24
+ns1=113
+ns2=175
+ns3=12
+print("Group numbers are", int(ns1/lg), int(ns2/lg), int(ns3/lg))
+print("Remainder from Groups are", ns1 % lg, ns2 % lg, ns3 % lg)
